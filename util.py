95535755d25b88551e93ce5fa0d1f0507ada9ec5
lchen025 <16787099+lchen025@users.noreply.github.com>
2020-07-08 20:48:18 -0400

    require zero-argument singleton construction

--- util.py
+++ util.py
@@ -2,16 +2,14 @@
 # Modified from https://gist.github.com/goutomroy/c925b1316bd9e7ec2cf9b1e4c183f5f4#file-singleton-py
 # Why is this not in some standard library? ¯\_(ツ)_/¯
 class Singleton:
-    def __init__(self, cls, *args, **kwargs):
+    def __init__(self, cls):
         self.cls = cls
-        self.args = args
-        self.kwargs = kwargs
 
     def get(self):
         try:
             return self.instance
         except AttributeError:
-            self.instance = self.cls(*self.args, **self.kwargs)
+            self.instance = self.cls()
             return self.instance
 
     def __call__(self):
